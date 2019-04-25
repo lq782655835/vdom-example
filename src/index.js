6604799d65cf3c1b7158a30b5c1098b6f74c4a7c
springleo <782655835@qq.com>
2019-04-25 14:03:20 +0800

    feat: render dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 引入createElement、render和renderDom方法
+import { createElement, render, renderDom } from './element';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let virtualDom = createElement('ul', {class: 'list'}, [
+    createElement('li', {class: 'item'}, ['周杰伦']),
+    createElement('li', {class: 'item'}, ['林俊杰']),
+    createElement('li', {class: 'item'}, ['王力宏'])
+]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(virtualDom);
+
+let el = render(virtualDom); // 渲染虚拟DOM得到真实的DOM结构
+console.log(el);
+
+// 直接将DOM添加到页面内
+renderDom(el, document.getElementById('root'));
